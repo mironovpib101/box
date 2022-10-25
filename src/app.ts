0079ed2b366443383ffa55dcf66826584b8ed658
Oleg <mironovpib101@gmail.com>
2022-10-25 07:38:33 +0400

    init frontend

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+export default class App {
+  public static async main(): Promise<void> {
+    console.log("RUN FRONTEND APP 2");
+  }
+}
+
+App.main();
---
+++ webpack/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/app.ts",
+  devServer: {
+    open: true,
+    compress: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    clean: true,
+    filename: "app.js",
+    path: path.resolve(__dirname, "../dist"),
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: "index.html",
+      template: path.resolve(__dirname, "../resource", "index.html"),
+    }),
+  ],
+};
